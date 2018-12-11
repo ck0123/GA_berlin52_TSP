4baf73c2c54739c83a34a1df3e4348f82a88c18c
易衍智 <occamscaidao.ck0123@gmail.com>
2018-12-11 17:53:55 +0900

    BASE update

---
+++ GA.py
@@ -0,0 +1,225 @@
+#The BASE GA algorithm is here:
+#https://towardsdatascience.com/evolution-of-a-salesman-a-complete-genetic-algorithm-tutorial-for-python-6fe5d2b3ca35
+
+import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
+import math
+
+
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, city):
+        xDis = abs(self.x - city.x)
+        yDis = abs(self.y - city.y)
+        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
+        return distance
+
+    def __repr__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+
+class Fitness:
+    def __init__(self, route):
+        self.route = route
+        self.distance = 0
+        self.fitness = 0.0
+
+    def routeDistance(self):
+        if self.distance == 0:
+            pathDistance = 0
+            for i in range(0, len(self.route)):
+                fromCity = self.route[i]
+                toCity = None
+                if i + 1 < len(self.route):
+                    toCity = self.route[i + 1]
+                else:
+                    toCity = self.route[0]
+                pathDistance += fromCity.distance(toCity)
+            self.distance = pathDistance
+        return self.distance
+
+    def routeFitness(self):
+        if self.fitness == 0:
+            dis = self.routeDistance()
+            self.fitness = dis
+        return self.fitness
+
+
+def createRoute(cityList):
+    route = random.sample(cityList, len(cityList))
+    return route
+
+def initialPopulation(popSize, cityList):
+    population = []
+
+    for i in range(0, popSize):
+        population.append(createRoute(cityList))
+    return population
+
+def rankRoutes(population):
+    fitnessResults = {}
+    for i in range(0,len(population)):
+        fitnessResults[i] = Fitness(population[i]).routeFitness()
+    return sorted(fitnessResults.items(), key = operator.itemgetter(1), reverse = False)
+
+
+def selection(popRanked, eliteSize):
+    selectionResults = []
+    for i in range(0, eliteSize):
+        selectionResults.append(popRanked[i][0])
+
+    popRanked_pre = popRanked[:len(popRanked)]
+    for i in range(0, len(popRanked) - eliteSize):
+        c1 = random.sample(popRanked_pre, 1)
+        c2 = random.sample(popRanked_pre, 1)
+        winner = None
+        if c1[0][1] > c2[0][1]:
+            winner = c1
+        else:
+            winner = c2
+        selectionResults.append(winner[0][0])
+
+    return selectionResults
+
+
+def matingPool(population, selectionResults):
+    matingpool = []
+    for i in range(0, len(selectionResults)):
+        index = selectionResults[i]
+        matingpool.append(population[index])
+    return matingpool
+
+
+def breed(parent1, parent2):
+    child = []
+    childP1 = []
+    childP2 = []
+
+    geneA = int(random.random() * len(parent1))
+    geneB = int(random.random() * len(parent1))
+
+    startGene = min(geneA, geneB)
+    endGene = max(geneA, geneB)
+
+    for i in range(startGene, endGene):
+        childP1.append(parent1[i])
+
+    childP2 = [item for item in parent2 if item not in childP1]
+
+    child = childP1 + childP2
+    return child
+
+
+def breedPopulation(matingpool, eliteSize):
+    children = []
+    length = len(matingpool) - eliteSize
+    pool = random.sample(matingpool, len(matingpool))
+
+    for i in range(0, eliteSize):
+        children.append(matingpool[i])
+
+    for i in range(0, length):
+        child = breed(pool[i], pool[len(matingpool) - i - 1])
+        children.append(child)
+    return children
+
+
+def mutate(individual, mutationRate):
+    for swapped in range(len(individual)):
+        if (random.random() < mutationRate):
+            swapWith = int(random.random() * len(individual))
+
+            city1 = individual[swapped]
+            city2 = individual[swapWith]
+
+            individual[swapped] = city2
+            individual[swapWith] = city1
+    return individual
+
+
+def mutatePopulation(population, mutationRate):
+    mutatedPop = []
+
+    for ind in range(0, len(population)):
+        mutatedInd = mutate(population[ind], mutationRate)
+        mutatedPop.append(mutatedInd)
+    return mutatedPop
+
+def nextGeneration(currentGen, eliteSize, mutationRate):
+    popRanked = rankRoutes(currentGen)
+    selectionResults = selection(popRanked, eliteSize)
+    matingpool = matingPool(currentGen, selectionResults)
+    children = breedPopulation(matingpool, eliteSize)
+    return children
+
+
+def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations):
+    pop = initialPopulation(popSize, population)
+    print("Initial distance: " + str(1 / rankRoutes(pop)[0][1]))
+
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+
+    print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
+    bestRouteIndex = rankRoutes(pop)[0][0]
+    bestRoute = pop[bestRouteIndex]
+    return bestRoute
+
+
+def plotting():
+    l1 = list()
+    for c in best:
+        l1.append([c.x,c.y])
+    l = np.asarray(l1)
+    plt.clf()
+    plt.scatter(l[:, 0].T, l[:, 1].T, s=10, c='k')
+    l1.append(l1[0])
+    l = np.asarray(l1)
+    plt.plot(l[:, 0].T, l[:, 1].T, 'r-')
+    # plt.show()
+    plt.savefig("berlin52_route.png")
+
+def read_line(s):
+    l = s.split(' ')
+    return float(l[0]), float(l[1]), float(l[2])
+
+
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations):
+    pop = initialPopulation(popSize, population)
+    progress = []
+    progress.append(rankRoutes(pop)[0][1])
+
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+        print(i)
+        progress.append(rankRoutes(pop)[0][1])
+
+    plt.clf()
+    plt.plot(progress)
+    plt.ylabel('Distance')
+    plt.xlabel('Generation')
+    # plt.show()
+    plt.savefig("berlin52_distance.png")
+
+    print("Final distance: " + str(rankRoutes(pop)[0][1]))
+    bestRouteIndex = rankRoutes(pop)[0][0]
+    bestRoute = pop[bestRouteIndex]
+    return bestRoute
+
+cityList = []
+with open('./TSP_data', 'rt') as f:
+    for line in f:
+        a, b, c = read_line(line)
+        cityList.append(City(x=b, y=c))
+
+
+best = geneticAlgorithmPlot(population=cityList, popSize=2000, eliteSize=1000, mutationRate=0.01, generations=2000)
+plotting()
+
+
+
+
+
+
